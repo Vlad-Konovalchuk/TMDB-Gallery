b03d9864d14e855139dc0b30435b13efade6dbba
Vlad Konovalchuk <berlitio600@gmail.com>
2018-12-23 23:47:44 +0200

    Add changes to moviedetailslayout styles

--- src/components/MovieDetails/MovieDetailsItemLayout.js
+++ src/components/MovieDetails/MovieDetailsItemLayout.js
@@ -2,80 +2,82 @@ import React from "react";
 import styles from "./MovieDetails.module.css";
 
 const MovieDetailsItemLayout = props => {
-  const {
-    crew = [],
-    budget,
-    companies = [],
-    countries = [],
-    genres = [],
-    originalTitle,
-    overview,
-    poster_path,
-    releaseDate
-  } = props.info;
-  return (
-    <div className={styles.movie__wrapper}>
-      <div className={styles.movie__poster}>
-        <img
-          src={`http://image.tmdb.org/t/p/w300${poster_path}`}
-          alt={originalTitle}
-        />
-      </div>
-      <div className={styles.movie__details}>
-        <h1 className={styles.movie__title}>{originalTitle}</h1>
-        <p className={styles.movie__overview}>
-          <h3 className={styles.sub__title}>Description:</h3>
-          {overview}
-        </p>
-        <ul className={styles.genre__list}>
-          <h3 className={styles.sub__title}>Genres:</h3>
-          {genres.map(item => (
-            <li className={styles.genre__list__item} key={item.id}>
-              {item.name},
-            </li>
-          ))}
-        </ul>
-        <p className={styles.movie__date}>
-          <h3 className={styles.sub__title}>Release Date: </h3>
-          {releaseDate}
-        </p>
-        <p>
-          <span className={styles.sub__title}> Movie Budget: </span>
-          {budget}
-        </p>
-        <div className={styles.movie__footer}>
-          <div className={styles.movie__company}>
-            <h3 className={styles.sub__title}>Companies</h3>
-            <ul className={styles.movie__list}>
-              {companies.map(item => (
-                <li className={styles.movie__list__item} key={item.id}>
-                  {item.name}
-                </li>
-              ))}
-            </ul>
-          </div>
-          <div className={styles.movie__countries}>
-            <h3 className={styles.sub__title}>Countries</h3>
-            <ul className={styles.movie__list}>
-              {countries.map(item => (
-                <li className={styles.movie__list__item} key={item.name}>
-                  {item.name}
-                </li>
-              ))}
-            </ul>
-          </div>
+    const {
+        crew = [],
+        budget,
+        production_countries = [],
+        production_companies = [],
+        genres = [],
+        originalTitle,
+        overview,
+        poster_path,
+        release_date,
+    } = props.info;
+    return (
+        <div className={styles.movie__wrapper}>
+            <div className={styles.movie__poster}>
+                <img
+                    src={`http://image.tmdb.org/t/p/w300${poster_path}`}
+                    alt={originalTitle}
+                />
+            </div>
+            <div className={styles.movie__details}>
+                <h1 className={styles.movie__title}>{originalTitle}</h1>
+                <p className={styles.movie__overview}>
+                    <h3 className={styles.sub__title}>Description:</h3>
+                    {overview}
+                </p>
+                <ul className={styles.genre__list}>
+                    <h3 className={styles.sub__title}>Genres:</h3>
+                    {genres.map(item => (
+                        <li className={styles.genre__list__item} key={item.id}>
+                            {item.name},
+                        </li>
+                    ))}
+                </ul>
+                <p className={styles.movie__date}>
+                    <h3 className={styles.sub__title}>Release Date: </h3>
+                    {release_date}
+                </p>
+                <p>
+                    <span className={styles.sub__title}> Movie Budget: </span>
+                    {budget}
+                </p>
+                <div className={styles.movie__footer}>
+                    <div className={styles.movie__company}>
+                        <h3 className={styles.sub__title}>Countries</h3>
+                        <ul className={styles.movie__list}>
+                            {production_countries.map(item => (
+                                <li className={styles.movie__list__item} key={item.id}>
+                                    {item.name}
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                    <div className={styles.movie__countries}>
+                        <h3 className={styles.sub__title}>Companies</h3>
+                        <ul className={styles.movie__list}>
+                            {production_companies.map(item => (
+                                <li className={styles.movie__list__item} key={item.name}>
+                                    {item.logo_path ?
+                                        <img src={`http://image.tmdb.org/t/p/w154${item.logo_path}`} alt=""/>
+                                        : item.name}
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                </div>
+                <div>
+                    <h3 className={styles.sub__title}>Crew List</h3>
+                    <ul>
+                        {crew.map(item => (
+                            <li key={item.id}>{item.name}</li>
+                        ))}
+                    </ul>
+                </div>
+            </div>
         </div>
-        <div>
-          <h3 className={styles.sub__title}>Crew List</h3>
-          <ul>
-            {crew.map(item => (
-              <li key={item.id}>{item.name}</li>
-            ))}
-          </ul>
-        </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default MovieDetailsItemLayout;
